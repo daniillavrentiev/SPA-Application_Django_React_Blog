1688db6d50a375554d42952cdadae138186402ee
Daniil <daniil.lavrentiev.00@gmail.com>
2021-05-19 21:21:15 +0300

    fef

--- main-ui/src/components/Category/CategoryDetail.js
+++ main-ui/src/components/Category/CategoryDetail.js
@@ -27,7 +27,7 @@ function CategoryDetail({ match }) {
     }, [id])
 
     return (
-            <Container fluid>
+            <Container>
             <Row>
                 <Col sm={8}>
                     <h3 className="pb-4 mb-4 fst-italic border-bottom">
